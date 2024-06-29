73923eea8aada0731eb6c9df76c5972fce9e7a4c
APOLLOL <lorenzo.apolloni@globallogic.com>
2024-06-29 13:43:46 +0300

    Added a new project & created an Enum for Labels

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,62 +1,69 @@
+import { Label } from '../enums/label.enum';
+
 export const projects = [
     {
         title: 'Weather App',
         description:
             'A React and TypeScript web app where you can check the weather for your current spot or look up forecasts for any city.',
         link: 'https://github.com/lorenzo-ap/weather-app',
-        label: 'Finished',
+        label: Label.Finished,
     },
     {
         title: 'Pulse',
         description: 'A sports store web app made with jQuery.',
         link: 'https://github.com/lorenzo-ap/pulse',
-        label: 'Finished',
+        label: Label.Finished,
     },
     {
         title: 'Writer Works',
         description: 'A web app made with pure JavaScript, offering full dissertation help.',
         link: 'https://github.com/lorenzo-ap/writer-works',
-        label: 'Finished',
+        label: Label.Finished,
     },
     {
         title: 'Wax',
         description: 'A JavaScript-powered app for seamless cryptocurrency transactions.',
         link: 'https://github.com/lorenzo-ap/wax',
-        label: 'Finished',
+        label: Label.Finished,
     },
     {
         title: 'Marketis',
         description: 'An all-in-one sneaker shopping platform, crafted with pure JavaScript.',
         link: 'https://github.com/lorenzo-ap/marketis',
-        label: 'Finished',
+        label: Label.Finished,
     },
     {
         title: 'Swipe',
         description: 'A JavaScript-based platform offering paid assistance to launch your business venture.',
         link: 'https://github.com/lorenzo-ap/swipe',
-        label: 'Finished',
+        label: Label.Finished,
     },
 ];
 
 export const misc = [
     {
+        title: 'Typing Game',
+        link: 'https://github.com/lorenzo-ap/typing-game',
+        label: Label.React,
+    },
+    {
         title: 'Budget App',
         link: 'https://github.com/lorenzo-ap/budget-app',
-        label: 'Angular',
+        label: Label.Angular,
     },
     {
         title: 'Kanban Board',
         link: 'https://github.com/lorenzo-ap/ng-kanban',
-        label: 'Angular',
+        label: Label.Angular,
     },
     {
         title: 'Robofriends',
         link: 'https://github.com/lorenzo-ap/robofriends',
-        label: 'React',
+        label: Label.React,
     },
     {
         title: 'Background Generator',
         link: 'https://github.com/lorenzo-ap/background-generator',
-        label: 'JavaScript',
+        label: Label.JavaScript,
     },
 ];
