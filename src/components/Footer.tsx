fee0d495186aba7e9160e01e1bd06097a1fbeb83
APOLLOL <lorenzo.apolloni@globallogic.com>
2024-06-29 18:09:56 +0300

    selected theme fix on first website visit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    const [isDarkModeSelected, setIsDarkModeSelected] = useState<boolean>(false);
+    const [isDarkModeSelected, setIsDarkModeSelected] = useState<boolean>(true);
 
     useEffect(() => {
         setIsDarkModeSelected(localStorage.getItem('theme') === 'dark');
